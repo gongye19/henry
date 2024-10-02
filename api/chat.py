f8a5ceb9b5f8cbba319bd68cd79f81f4606a9371
gongye19 <gy723511500@gmail.com>
2024-10-02 17:02:53 +0800

    flow2

--- api/chat.py
+++ api/chat.py
@@ -16,14 +16,9 @@ system_prompt = '''你需要扮演一个人，中文名字叫做朱晗，英文�
 年龄：27。
 性别：男。
 教育背景：本科就读于杭州电子科技大学，通信工程专业；第一个硕士就读于四川大学，人工智能专业；目前第二个硕士就读于香港理工大学，数据科学专业。去香港理工大学读第二个硕士的原因是当时在四川大学的实验室项目偏横向项目，同学都去找后端开发方面的工作，但自己的兴趣还是在AI方面，想从事算法方面的工作。
-兴趣爱好：喜欢户外运动比如，徒步，登山；体育运动喜欢踢球，喜欢打羽毛球；其他空闲时间喜欢电影，动漫，音乐，编曲，弹钢琴，弹吉他。
+兴趣爱好：喜欢户外运动比如，徒步，登山；体育运动喜欢踢球，喜欢打羽毛球；其他空闲时间喜欢电影，动漫，音乐��编曲，弹钢琴，弹吉他。
 个人情况：单身没有女朋友，有喜欢的女生，但是那个女生不喜欢他。喜欢旅行，喜欢和简单善良的人做朋友。音乐方面喜欢爵士音乐，嘻哈音乐。
 未来计划：短期计划是做好大语言模型，AIGC方面的工作和研究，长期计可能会考虑从事STEAM方向，PYP教育方面的工作。'''
-
-def stream_response(response):
-    for chunk in response:
-        if chunk.choices[0].delta.content is not None:
-            yield chunk.choices[0].delta.content
 
 def handle_request(query):
     try:
@@ -36,8 +31,9 @@ def handle_request(query):
             stream=True
         )
 
-        for chunk in stream_response(response):
-            yield f"data: {json.dumps({'content': chunk})}\n\n"
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                yield f"data: {json.dumps({'content': chunk.choices[0].delta.content})}\n\n"
 
         yield "data: [DONE]\n\n"
     except Exception as e:
